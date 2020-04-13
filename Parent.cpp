13d1f52ffdbe998e81b7d98d2744a47f617a6613
Saurav <sauravc41@gmail.com>
2020-04-12 18:21:01 -0700

    More updates

--- Parent.cpp
+++ Parent.cpp
@@ -6,20 +6,23 @@
 using namespace std;
 
 void printMenu();
+inline void update();
+
+int choice, sizea, count;
+char letter;
+char* array;
+long childPID;
+    
 
 int main() {
-    int choice, size, count;
-    char letter;
-    char* array;
-    long childPID;
-    count = 0;
+    //count = 0;
     
     printMenu();
     cin >> choice;
     do {
         if (choice == 1) {
             cout << "How big is the array?" << endl;
-            cin >> size;
+            cin >> sizea;
             
             cout << "what letter?" << endl;
             cin >> letter;
@@ -49,50 +52,49 @@ int main() {
                 cout << "Child process: pid " << getpid() << ", ppid "
                 << getppid() << ", child " << childPID << "\n" << endl;
 
-                array = new char[size]; //allocate memory for array
+                array = new char[sizea]; //allocate memory for array
 
                 if (choice == 2) {
                     break;
                 }
 
-                cout << size << endl;
+                cout << sizea << endl;
 
                 //fill array with random letters
-                for (int i = 0; i < size; i++) {
+                for (int i = 0; i < sizea; i++) {
                     array[i] = (rand() % 26) + 'A';
                     //cout << array[i] << endl;
                 }
 
                 int i = 0;
                 //look for letter and count
-                for (i = 0; i < size; i++) {
-                    there:
-                    if (array[i] == letter) {
-                        count++;
-                        //spawn child
-                        childPID = fork();
-                        if (childPID > 0) { //parent
+                for (i = 0; i < sizea; i++) {
+                    childPID = fork();
+                    if (childPID > 0) { //parent
                             wait(NULL);
-                        }
-                        if (childPID == 0) { //child
-                            //cout << "child made" << endl;
-                            if (array[i] == letter) {
-                                count++;
-                                //cout << "exit child" << endl;
-                                exit(1);
-                            }
-                        }
+                            //exit(1);
                     }
+                    if (childPID == 0) { //child
+                        cout << "child made" << endl;
+                        if (array[i] == letter) {
+                            goto here;
+                            there:
+                            cout << "up" << endl;
+                        }
+                        cout << "exit child" << endl;
+                        exit(1);
+                        
+                        
+                    }
+                    
                 }
-                if (count == 0) { //injected bug, restart search
-                    cout << "Not found, restarting" << endl;
-                    cout << "Run kill command on: " << getpid() << endl;
-                    i = 0;
-                    sleep(1); //sleep for a second to see the PID in console
+                if (false) {
+                    here:
+                    update();
                     goto there;
                 }
-                cout << "HERE" << endl;
-                cin >> size;
+                
+                
                 cout << "count is " << count << endl;
                 cout << "exit last child" << endl;
                 exit(1);
@@ -110,4 +112,8 @@ void printMenu() {
     cout << "What would you like to do?" << endl;
     cout << "1) Run the program" << endl;
     cout << "2) Exit the program" << endl;
+}
+
+inline void update() {
+    count++;
 }
