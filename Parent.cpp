96e3bb668d016121eba0bd9f79be73f0620a952e
Saurav <sauravc41@gmail.com>
2020-04-12 18:59:58 -0700

    Updated behavior of child spawning and ending

--- Parent.cpp
+++ Parent.cpp
@@ -6,31 +6,28 @@
 using namespace std;
 
 void printMenu();
-inline void update();
 
 int choice, sizea, count;
 char letter;
 char* array;
 long childPID;
     
-
 int main() {
-    //count = 0;
-    
     printMenu();
     cin >> choice;
     do {
         if (choice == 1) {
-            cout << "How big is the array?" << endl;
+            cout << "\nHow big is the array?" << endl;
             cin >> sizea;
             
-            cout << "what letter?" << endl;
+            cout << "What letter?" << endl;
             cin >> letter;
 
             //spawn child
             childPID = fork();
 
             if (childPID > 0) { //parent
+                cout << "" << endl;
                 cout << "Parent process: pid " << getpid() << ", ppid " 
                 << getppid() << ", child " << childPID << "\n" << endl;
                 
@@ -40,25 +37,26 @@ int main() {
                     break;
                 }
                 else {
-                    cout << "here" << endl;
+                    cout << "back to parent" << endl;
+                    cout << "" << endl;
                     printMenu();
                     cin >> choice;
-                    cout << endl;
+                    //cout << endl;
                     delete array; //free up memory
                     continue;
                 }
             }
             else if (childPID == 0) { //child
+                if (choice == 2) {
+                    break;
+                }
+
                 cout << "Child process: pid " << getpid() << ", ppid "
                 << getppid() << ", child " << childPID << "\n" << endl;
 
                 array = new char[sizea]; //allocate memory for array
 
-                if (choice == 2) {
-                    break;
-                }
-
-                cout << sizea << endl;
+                //cout << "Size of array is: " << sizea << "\n" << endl;
 
                 //fill array with random letters
                 for (int i = 0; i < sizea; i++) {
@@ -66,37 +64,17 @@ int main() {
                     //cout << array[i] << endl;
                 }
 
-                int i = 0;
-                //look for letter and count
+                //Search through array and count
+                int i;
                 for (i = 0; i < sizea; i++) {
-                    childPID = fork();
-                    if (childPID > 0) { //parent
-                            wait(NULL);
-                            //exit(1);
+                    if (array[i] == letter) {
+                        count++;
                     }
-                    if (childPID == 0) { //child
-                        cout << "child made" << endl;
-                        if (array[i] == letter) {
-                            goto here;
-                            there:
-                            cout << "up" << endl;
-                        }
-                        cout << "exit child" << endl;
-                        exit(1);
-                        
-                        
-                    }
-                    
                 }
-                if (false) {
-                    here:
-                    update();
-                    goto there;
-                }
-                
-                
+
                 cout << "count is " << count << endl;
-                cout << "exit last child" << endl;
+                cout << "" << endl;
+                cout << "exit child" << endl;
                 exit(1);
             }
             else {
@@ -110,10 +88,8 @@ int main() {
 
 void printMenu() {
     cout << "What would you like to do?" << endl;
+    cout << "==========================" << endl;
     cout << "1) Run the program" << endl;
     cout << "2) Exit the program" << endl;
-}
-
-inline void update() {
-    count++;
+    cout << "==========================" << endl;
 }
